5433e84383270ec5db748bfef263f05474749fc8
Adrian <114823878+Imdriann@users.noreply.github.com>
2024-10-29 18:10:56 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Muhammad Adrian Dwiharyanto" // 
+const studentId = "FS10780271" // TODO: replace this
+
+export { url, studentName, studentId }
